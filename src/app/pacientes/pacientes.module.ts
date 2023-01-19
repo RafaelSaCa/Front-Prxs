543c909225bc25459f0425ce4e885acacb1b0d44
RafaelSaCa <raffaellsaca@gmail.com>
2023-01-18 23:17:27 -0300

    Formulario Paciente

--- src/app/pacientes/pacientes.module.ts
+++ src/app/pacientes/pacientes.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { SharedModule } from './../shared/shared.module';
+import { PacienteFormComponent } from './paciente-form/paciente-form.component';
 import { PacientesRoutingModule } from './pacientes-routing.module';
 import { PacientesComponent } from './pacientes/pacientes.component';
-import { PacienteFormComponent } from './paciente-form/paciente-form.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { PacienteFormComponent } from './paciente-form/paciente-form.component';
     CommonModule,
     PacientesRoutingModule,
     AppMaterialModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class PacientesModule { }
