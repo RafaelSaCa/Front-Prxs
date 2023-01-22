5ca03ccf8180c092eb17a9617f5fb58dd02310b6
RafaelSaCa <raffaellsaca@gmail.com>
2023-01-22 18:32:29 -0300

    Atualizando os dados do Paciente

--- src/app/pacientes/services/pacientes.service.ts
+++ src/app/pacientes/services/pacientes.service.ts
@@ -15,15 +15,27 @@ export class PacientesService {
   list() {
     return this.httpClient.get<Paciente[]>(this.API).pipe(
       first(),
-     // delay(1500),
+      // delay(1500),
       tap((pacientes) => console.log(pacientes))
     );
   }
 
-  loadFormById(id: string){
-   return this.httpClient.get<Paciente>(`${this.API}/${id}`);
+  loadFormById(id: string) {
+    return this.httpClient.get<Paciente>(`${this.API}/${id}`);
   }
-  save(record: Partial<Paciente>){//Partial permite passar atributos parciais do objeto. ex: nome, cpf, sem os demais atributos.
+
+  save(record: Partial<Paciente>) {//Partial permite passar atributos parciais do objeto. ex: nome, cpf, sem os demais atributos.
+    if (record._id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Partial<Paciente>) {
     return this.httpClient.post<Paciente>(this.API, record);
+  }
+
+  private update(record: Partial<Paciente>) {
+    return this.httpClient.put<Paciente>(`${this.API}/${record._id}`, record);
   }
 }
